ed7575f7e6f97b5a9dfbdfd1317fa3ed367696bd
ConeHead69 <113296892+ConeHead69@users.noreply.github.com>
2022-11-06 10:27:50 -0500

    Finished Junior_easy_1 challenge

--- Challenge/Harrison_challenge/Junior_easy_1.py
+++ Challenge/Harrison_challenge/Junior_easy_1.py
@@ -2,18 +2,18 @@ import random
 shot_a3 = (random.randint(0,100))
 shot_a2 = (random.randint(0,100))
 shot_a1= (random.randint(0,100))
-print(shot_a3, shot_a2, shot_a1)
+print(f"The Apples scored {shot_a3} 3-point shots, {shot_a2} 2-point shots and {shot_a1} 1-point shots")
 
 shot_b3 = (random.randint(0,100))
 shot_b2 = (random.randint(0,100))
 shot_b1= (random.randint(0,100))
-print(shot_b3, shot_b2, shot_b1)
+print(f"The Bananas scored {shot_a3} 3-point shots, {shot_a2} 2-point shots and {shot_a1} 1-point shots")
 
-total = shot_a3 + shot_a2 + shot_a1
-print(total)
+total = shot_a3*3 + shot_a2*2 + shot_a1
+print(f"The Apples got a grand score of {total}")
 
-total2 = shot_b3 + shot_b2 + shot_b1
-print(total2)
+total2 = shot_b3*3 + shot_b2*2 + shot_b1
+print(f"The Bananas got a grand score of {total2}")
 
 if total > total2:
     print("The Apples have won the game!")
