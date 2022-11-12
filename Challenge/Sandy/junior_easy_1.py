5507856ed2f57ab104a7e82183dff46f43f56089
Sandysiyao <113297205+Sandysiyao@users.noreply.github.com>
2022-11-12 17:02:41 -0500

    python

---
+++ Challenge/Sandy/junior_easy_1.py
@@ -0,0 +1,11 @@
+apples3 = input("How much 3 points apples scored")
+apples2 = input("How much 2 points apples scored")
+apples1 = input("How much 1 points apples scored")
+apples_score = apples3*3 + apples2*2 + apples1*1
+print(int('apples_score'))
+
+banana3 = input("How much 3 points banana scored")
+banana2 = input("How much 2 points banana scored")
+banana1 = input("How much 1 points banana scored")
+banana_score = banana3*3 + banana2*2 + banana1*1
+print(int('banana_score'))
