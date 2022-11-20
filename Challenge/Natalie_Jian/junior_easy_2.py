f4f94134003790d68ac4a80d562eeb3823ce6f19
PingTheGreat <100525577+PingTheGreat@users.noreply.github.com>
2022-11-20 10:21:15 -0500

    Updated Junior easy challenge 2
    after listening to the thought process and mini solution, I need to re work on it since I did it wrong

--- Challenge/Natalie_Jian/junior_easy_2.py
+++ Challenge/Natalie_Jian/junior_easy_2.py
@@ -2,29 +2,13 @@ with open("c2.txt", "r") as f:
     lines = f.readlines()
 
 a = lines[0]
+
+
 b = lines[1]
-c = lines[2]
-d = lines[3]
-e = lines[4]
+b.strip("\n")
+b.split(" ")
+decrypted = int(b[0])*b[1]
+print(decrypted)
+    
 
-
-print(a)
-print(b)
-print(c)
-print(d)
-print(e)
-
-print(" ")
-print("Output equals")
-print(" ")
-
-line_b = (9 * "+")
-line_c = (3 * "-")
-line_d = (12 * "A")
-line_e = (2 * "X")
-
-print(line_b)
-print(line_c)
-print(line_d)
-print(line_e)
-
+    
