2aeeb3553fdaf9d7e260b33d7ccfe6d85b4ae852
PingTheGreat <100525577+PingTheGreat@users.noreply.github.com>
2022-11-20 09:36:53 -0500

    Finished junior easy challenge 2

--- Challenge/Natalie_Jian/junior_easy_2.py
+++ Challenge/Natalie_Jian/junior_easy_2.py
@@ -6,10 +6,25 @@ b = lines[1]
 c = lines[2]
 d = lines[3]
 e = lines[4]
-if "\n" in a:
-    a.strip('\n')
-    print(a)
-    print(b)
-    print(c)
-    print(d)
-    print(e)
+
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+print(" ")
+print("Output equals")
+print(" ")
+
+line_b = (9 * "+")
+line_c = (3 * "-")
+line_d = (12 * "A")
+line_e = (2 * "X")
+
+print(line_b)
+print(line_c)
+print(line_d)
+print(line_e)
+
