a13b4d0dd60efe5e761cde81a627d323c7387fcb
NotJohnsonC <101054552+NotJohnsonC@users.noreply.github.com>
2022-11-27 11:00:01 -0500

    finished decode msg project putting the list into a new file

--- Challenge/Johnson ccc question code/Junior_easy_2/Junioe_easy_2.py
+++ Challenge/Johnson ccc question code/Junior_easy_2/Junioe_easy_2.py
@@ -1,10 +1,18 @@
-l = open("decode_message.txt", "r")
-l2 = int(l.readline().strip("\n"))
-for i in range(l2):
-    l3 = l.readline()
-    if "\n" in l3:
-        strip1 = l3.strip("\n")
+dcode_msg_file = open("decode_message.txt", "r")
+n_of_msg = int(dcode_msg_file.readline().strip("\n"))
+dcoded_msg_lst = []
+for i in range(n_of_msg):
+    nextline = dcode_msg_file.readline()
+    if "\n" in nextline:
+        strip1 = nextline.strip("\n")
     else:
-        strip1 = l3
-    l4 = strip1.split(" ")
-    print(l4[1] * int(l4[0]))
+        strip1 = nextline
+    split_dcode_msg = strip1.split(" ")
+    dcoded_msg = split_dcode_msg[1] * int(split_dcode_msg[0])
+    dcoded_msg_lst.append(dcoded_msg + "\n")
+dcode_msg_file.close() 
+
+with open("dcode_msg_looplist.txt", 'w') as f:
+    for i in dcoded_msg:
+        nfile = open("dcode_msg_looplist.txt", 'w')
+        nfile.writelines(dcoded_msg_lst)
