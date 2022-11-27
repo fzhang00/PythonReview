86fd2b8a92f9092711ed836ba2a52aeb6713703c
PingTheGreat <100525577+PingTheGreat@users.noreply.github.com>
2022-11-27 10:27:03 -0500

    Junior easy challenge 2

--- Challenge/Natalie_Jian/junior_easy_2.py
+++ Challenge/Natalie_Jian/junior_easy_2.py
@@ -1,14 +1,41 @@
 with open("c2.txt", "r") as f:
     lines = f.readlines()
 
-a = lines[0]
+a = int(lines[0])
 
+b1 = lines[1]
+b_strip = b1.strip("\n")
+b_line = b_strip.split(" ")
+b = int(b_line[0])*(b_line[1])
 
-b = lines[1]
-b.strip("\n")
-b.split(" ")
-decrypted = int(b[0])*b[1]
-print(decrypted)
     
+c1 = lines[2]
+c_strip = c1.strip("\n")
+c_line = c_strip.split(" ")
+c = int(c_line[0])*(c_line[1])
 
+d1 = lines[3]
+d_strip = d1.strip("\n")
+d_line = d_strip.split(" ")
+d = int(d_line[0])*(d_line[1])
+
+e1 = lines[4]
+e_strip = e1.strip("\n")
+e_line = e_strip.split(" ")
+e = int(e_line[0])*(e_line[1])
+
+# decode the first line in the file to get number_of_message out of this line
+number_of_message = 5
+# repeat for number_of_message time by using for loop
+# for i in range(number_of_message):
+    # read another line, or reand the next element in your list
+    # strip
+    # split
+    # decoding
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
     
