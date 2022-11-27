b13552dfefe27906e11a531f07b0d1133292bdbc
Richthepythonmaster <105087821+Richthepythonmaster@users.noreply.github.com>
2022-11-27 09:05:07 -0500

    I have added some new parts

--- Challenge/Richard/junior_easy_2.py
+++ Challenge/Richard/junior_easy_2.py
@@ -1,2 +1,9 @@
 f = open('junior_easy_2.txt', 'r')
-f.readline()
+line = f.readline() #number of lines of messages
+lines_of_messages = int(line.strip('\n'))
+for i in lines_of_messages:
+    f.readline()
+    code = line.strip('\n')
+    (number_part, character_part) = code.split(' ')
+    number_part = int(number_part)
+    print(number_part * character_part)
