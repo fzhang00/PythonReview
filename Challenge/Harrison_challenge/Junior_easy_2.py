935b7fecf76e1e5135e889f44b5cb2423914e202
ConeHead69 <113296892+ConeHead69@users.noreply.github.com>
2022-12-11 10:21:28 -0500

    Junior_easy_2 pt2

--- Challenge/Harrison_challenge/Junior_easy_2.py
+++ Challenge/Harrison_challenge/Junior_easy_2.py
@@ -1,13 +1,18 @@
 with open("C2.txt", "r") as f:
-    line = f.readline()
+    line = f.readlines()
 
 total_messages = int(line[0])
 
-for i in range(1, total_messages):
-    input_a = line
-    input_b = input_a.strip("\n")
-    input_c = input_b.split(" ")
-    total = int(input_c[0])*(input_c[1])
+for i in range(1, total_messages+1):
+    print(i)
+    input = line[i].strip("\n") #line[i] grabs the value at index i
+    input_b = input.split(" ")
+    total = int(input_b[0])*(input_b[1])
+    print(total)
+    written_total = list(total[i])
+
+with open("Output_C2.txt", "w") as f:
+    f.writelines(written_total)
 
 
 # Repeat for n times using for loop
