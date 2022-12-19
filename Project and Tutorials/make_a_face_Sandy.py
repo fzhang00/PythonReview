5caa182cf6e60cfb865d754b6b2049dc29892b55
Sandysiyao <113297205+Sandysiyao@users.noreply.github.com>
2022-12-19 17:13:28 -0500

    Update make_a_face_Sandy.py

--- Project and Tutorials/make_a_face_Sandy.py
+++ Project and Tutorials/make_a_face_Sandy.py
@@ -9,5 +9,11 @@ def draw():
     width=400
     height=400
     ellipse((width/2, height/2), 200, 200)
+    stroke(0)
+    fill(255, 226, 208)
+    fill(0, 0, 0)
+    eye_size = 50
+    ellipse((160, 180), eye_size, eye_size)
+    ellipse((240, 180), eye_size, eye_size)
 
 run()
